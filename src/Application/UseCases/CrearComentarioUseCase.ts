62b2544e181325b7249a8bf02baf1cab007d0930
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-07-21 12:52:28 -0600

    fallo de kk

--- src/Application/UseCases/CrearComentarioUseCase.ts
+++ src/Application/UseCases/CrearComentarioUseCase.ts
@@ -1,5 +1,6 @@
 import { Comentario } from "../../Domain/Entities/Comentario";
 import { UsuarioRepository } from "../../Domain/Repositories/Ports";
+import crypto from "crypto";
 
 export class CrearComentarioUseCase {
   constructor(private readonly usuarioRepository: UsuarioRepository) {}
