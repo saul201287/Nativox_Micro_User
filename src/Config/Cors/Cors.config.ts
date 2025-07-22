c45328cded6d432093588c91c6ce4caa14260eae
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-07-21 18:48:31 -0600

    chance

--- src/Config/Cors/Cors.config.ts
+++ src/Config/Cors/Cors.config.ts
@@ -1,6 +1,7 @@
 export const corsOptions = {
-  origin: ["*", " http://localhost:52171"],
+  origin: ["http://localhost:52171"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
   optionsSuccessStatus: 200,
 };
